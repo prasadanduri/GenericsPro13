25a89e0b2978069f7c0244e7e4e43f530221aaea
prasad <prasadanduri13gmail.com>
2022-03-20 21:26:38 +0530

    check Max Number

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsPro13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxNumCheck Max = new MaxNumCheck();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to Generic maximum problems");
+                Console.WriteLine("Enter choice of program : \n1 . Maximum integer \n2. Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter first number, second number,third number");
+                        int firstnumber = Convert.ToInt32(Console.ReadLine());
+                        int secondnumber = Convert.ToInt32(Console.ReadLine());
+                        int thirdnumber = Convert.ToInt32(Console.ReadLine());
+                        int result = Max.Computemax(firstnumber, secondnumber, thirdnumber);
+                        Console.WriteLine("The maximum number is : " + result);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+
+                }
+            }
+        }
+    }
+}
+      
